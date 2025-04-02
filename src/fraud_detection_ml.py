bf3f77675879ea7a0773ea57d3588af249bbb71c
captmicr0 <92401273+captmicr0@users.noreply.github.com>
2025-04-02 10:14:34 -0400

    finished NON-SMOTE testing

--- src/fraud_detection_ml.py
+++ src/fraud_detection_ml.py
@@ -1,12 +1,14 @@
 import os
 import argparse
 import pandas as pd
+from sklearn.discriminant_analysis import StandardScaler
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sqlalchemy import create_engine
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.calibration import LabelEncoder
 from xgboost import XGBClassifier
 from sklearn.metrics import (classification_report, 
                             roc_curve, 
@@ -43,7 +45,7 @@ class FraudDetector:
         
         return df_train, df_test
 
-    def visualize_missing_values(self, df):
+    def visualize_missing_values(self, df, fn='missing_values_bar_graph.png'):
         """Generate visualizations for EDA."""
 
         missing_values = df.isnull().sum()
@@ -55,10 +57,10 @@ class FraudDetector:
         plt.ylabel('Number of Missing Values')
         plt.xticks(rotation=45)
         plt.tight_layout()
-        plt.savefig(os.path.join(self.eda_visuals_path, 'missing_values_bar_graph.png'))
+        plt.savefig(os.path.join(self.eda_visuals_path, fn))
         plt.close()
 
-    def visualize_is_fraud(self, y):
+    def visualize_is_fraud(self, y, fn='is_fraud_distribution.png'):
         """Generate a bar graph showing the distribution of the target variable (is_fraud)."""
 
         plt.figure(figsize=(8, 6))
@@ -68,23 +70,18 @@ class FraudDetector:
         plt.ylabel('Count')
         plt.xticks(rotation=0)
         plt.tight_layout()
-        plt.savefig(os.path.join(self.eda_visuals_path, 'is_fraud_distribution.png'))
+        plt.savefig(os.path.join(self.eda_visuals_path, fn))
         plt.close()
 
     def train_model(self, test_size=0.2, train_table='fraud_train', test_table='fraud_test'):
         """Train and evaluate model with class balancing"""
         # Load data and select features
         df_train, df_test = self.load_data(train_table, test_table)
-        
+        X_train, y_train, X_test, y_test = self.feature_engineering(df_train, df_test)
+
         # Handle imbalance
-        #smote = SMOTE(sampling_strategy=0.1, random_state=42)
-        smote = SMOTE(random_state=42)
-        X_res, y_res = smote.fit_resample(X, y)
-        
-        # Split data
-        X_train, X_test, y_train, y_test = train_test_split(
-            X_res, y_res, test_size=test_size, random_state=42
-        )
+        smote = SMOTE(sampling_strategy="auto", random_state=42)
+        X_res, y_res = smote.fit_resample(X_train, y_train)
         
         # Model training
         self.model = XGBClassifier(
@@ -93,17 +90,19 @@ class FraudDetector:
             max_depth=5
         )
         self.model.fit(X_train, y_train)
-        
-        # Evaluate
-        y_pred = self.model.predict(X_test)
+
+        # Predict test data
+        prediction = self.model.predict(X_test)
+
+        # Evaluate and create visualizations
         print("\nModel Evaluation:")
-        print(classification_report(y_test, y_pred))
-        print(f"AUC-ROC Score: {roc_auc_score(y_test, y_pred):.2f}")
-        
-        # Generate visualizations
+        print(classification_report(y_test, prediction))
+        print(f"AUC-ROC Score: {roc_auc_score(y_test, prediction):.2f}")
+
         self._plot_roc_curve(y_test, self.model.predict_proba(X_test)[:,1])
-        self._plot_confusion_matrix(y_test, y_pred)
-        self._plot_feature_importance(X)
+        self._plot_confusion_matrix(y_test, prediction)
+
+        self._plot_feature_importance(X_train)
     
     def feature_engineering(self, df_train, df_test):
         # Drop columns that are not important
@@ -118,9 +117,56 @@ class FraudDetector:
 
         X_test = df_test.drop('is_fraud', axis=1)  # features
         y_test = df_test['is_fraud']  # target
-        
 
-    def _plot_roc_curve(self, y_true, y_probs):
+        # Recognize numerical and categorical features in the training data
+        numerical_cols = X_train.select_dtypes(include=[np.number]).columns.tolist()
+        categorical_cols = X_train.select_dtypes(include=['object']).columns.tolist()
+        
+        # Extract features from transaction date and time, then remove original column
+        X_train['trans_date_trans_time'] = pd.to_datetime(X_train['trans_date_trans_time'])
+        X_test['trans_date_trans_time'] = pd.to_datetime(X_test['trans_date_trans_time'])
+        
+        X_train['hour'] = X_train['trans_date_trans_time'].dt.hour
+        X_train['day'] = X_train['trans_date_trans_time'].dt.day
+        X_train['month'] = X_train['trans_date_trans_time'].dt.month
+
+        X_test['hour'] = X_test['trans_date_trans_time'].dt.hour
+        X_test['day'] = X_test['trans_date_trans_time'].dt.day
+        X_test['month'] = X_test['trans_date_trans_time'].dt.month
+        
+        X_train.drop('trans_date_trans_time', axis=1, inplace=True, errors='ignore')
+        X_test.drop('trans_date_trans_time', axis=1, inplace=True, errors='ignore')
+
+        # Update after processing
+        categorical_cols = X_train.select_dtypes(include=['object']).columns.tolist()
+        numerical_cols = X_train.select_dtypes(include=[np.number]).columns.tolist()
+
+        # Convert categories into usable data
+        label_encoder = LabelEncoder()
+        for col in categorical_cols:
+            # avoid unseen labels
+            combined = pd.concat([X_train[col], X_test[col]])
+            label_encoder.fit(combined)
+
+            # transform data
+            X_train[col] = label_encoder.transform(X_train[col])
+            X_test[col] = label_encoder.transform(X_test[col])
+        
+        # Scale features
+        scaler = StandardScaler()
+        X_train[numerical_cols] = scaler.fit_transform(X_train[numerical_cols])
+        X_test[numerical_cols] = scaler.transform(X_test[numerical_cols])  # for test data don't use fit_transform
+
+        # Make sure both sets of data have matching columns
+        X_train, X_test = X_train.align(X_test, join='inner', axis=1)
+
+        print("Feature set of training and testing data:")
+        print(X_train.columns)
+
+        # Return processed data
+        return X_train, y_train, X_test, y_test
+
+    def _plot_roc_curve(self, y_true, y_probs, fn='roc_curve.png'):
         """Generate ROC curve visualization"""
         fpr, tpr, _ = roc_curve(y_true, y_probs)
         plt.figure()
@@ -130,10 +176,10 @@ class FraudDetector:
         plt.ylabel('True Positive Rate')
         plt.title('ROC Curve')
         plt.legend(loc="lower right")
-        plt.savefig(os.path.join(self.model_visuals_path, 'roc_curve.png'))
+        plt.savefig(os.path.join(self.model_visuals_path, fn))
         plt.close()
 
-    def _plot_confusion_matrix(self, y_true, y_pred):
+    def _plot_confusion_matrix(self, y_true, y_pred, fn='confusion_matrix.png'):
         """Generate confusion matrix visualization"""
         cm = confusion_matrix(y_true, y_pred)
         plt.figure(figsize=(6,6))
@@ -141,10 +187,10 @@ class FraudDetector:
         plt.title('Confusion Matrix')
         plt.ylabel('True Label')
         plt.xlabel('Predicted Label')
-        plt.savefig(os.path.join(self.model_visuals_path, 'confusion_matrix.png'))
+        plt.savefig(os.path.join(self.model_visuals_path, fn))
         plt.close()
 
-    def _plot_feature_importance(self, X):
+    def _plot_feature_importance(self, X, fn='feature_importance.png'):
         """Generate feature importance visualization."""
         # Get feature importances from the model
         importances = self.model.feature_importances_
@@ -172,7 +218,7 @@ class FraudDetector:
         plt.tight_layout()
         
         # Save and show plot
-        plt.savefig(os.path.join(self.model_visuals_path, 'feature_importance.png'))
+        plt.savefig(os.path.join(self.model_visuals_path, fn))
         plt.close()
 
     def save_model(self, path):
